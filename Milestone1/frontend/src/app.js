0337957902163345bcd3d1d24172ba9f604a4f11
Deci Horine <djhorine@ncsu.edu>
2023-10-15 04:18:35 -0400

    Add template for frontend routers

--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const homeRouter = require('./homeRouter')
 
 const app = express();
 const PORT = process.env.PORT;
@@ -6,6 +7,8 @@ const PORT = process.env.PORT;
 // Designate the static folder as serving static resources
 app.use(express.static(__dirname + '/static'));
 
+// Set up routers
+app.use('/', homeRouter);
 
 // As our server to listen for incoming connections
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
