b9a0c00cc7f2473f633a3e782302293535df2c68
Deci Horine <djhorine@ncsu.edu>
2023-10-16 17:40:55 -0400

    Recreate project in Vite

---
+++ Milestone1/frontend/src/vite.config.js
@@ -0,0 +1,17 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'home/index.html'),
+                note: resolve(__dirname, 'note/index.html'),
+                registration: resolve(__dirname, 'registration/index.html'),
+                signin: resolve(__dirname, 'signin/index.html'),
+                demo: resolve(__dirname, 'demo/index.html')
+            }
+        }
+    }
+})
