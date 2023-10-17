b96b322d297988605ee6c65566b4f7f889885ce9
Nam Pham <pthanhn@ncsu.edu>
2023-10-17 10:58:46 -0400

    New Note interface using TinyMCE

---
+++ Milestone1/frontend/src/note/script.js
@@ -0,0 +1,12 @@
+tinymce.init({
+    selector: 'textarea#default',
+    menubar: 'file edit view insert format',
+    toolbar: 'undo redo | fontfamily fontsize| bold italic underline| alignleft aligncenter alignright alignjustify | ' +
+    'bullist numlist outdent indent | link image | preview media fullscreen | ' +
+    ' forecolor backcolor emoticons',
+    plugins: 'image',
+    a11y_advanced_options: true,
+    promotion: false,
+    branding: false,
+    width: '50%'
+});
