36e37b93c3c1fc4c6b931e0ab4122ba2efb690e5
Deci Horine <djhorine@ncsu.edu>
2023-10-17 11:51:47 -0400

    Re-add files lost in a git error

---
+++ Milestone1/api/api/src/example/notesRouter-example.js
@@ -0,0 +1,17 @@
+let examples = {};
+
+examples.authToken = "afj93sfjkljawef";
+
+examples.notes = [
+    {"noteID": "1", 
+    "title": "Lecture #1", 
+    "content": "Note created sucessfully  ", 
+    "tags":["Course A", "Professor A"] },
+    
+    {"noteID": "10", 
+    "title": "Lecture #10", 
+    "content": "Note created sucessfully  ", 
+    "tags":["Course B", "Professor B"] }
+];
+
+module.exports = examples
