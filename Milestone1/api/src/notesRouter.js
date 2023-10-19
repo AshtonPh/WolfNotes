dac1d3313d5d435afb834bd36220d460cc091e71
Nam Pham <pthanhn@ncsu.edu>
2023-10-19 11:06:13 -0400

    Notes router improve

--- Milestone1/api/src/notesRouter.js
+++ Milestone1/api/src/notesRouter.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const app = express();
 const router = express.Router();
 const example = require("./example/notesRouter-example");
 
 //Middleware to parse JSON request bodies
-app.use(express.json());
+//router.use(express.json());
 
 
 function authenticate(req) {
@@ -18,8 +17,55 @@ function authenticate(req) {
     }
 }
 
+// Search notes by tags
+router.get("/searchByTag", (req, res) => {
+  if (!authenticate(req))
+      return;
+  const { tag } = req.query;
+
+  // Split the tag parameter into an array of tags
+  const tags = tag.split(",");
+
+  // Filter the notes based on the tags
+  const filteredNotes = example.notes.filter((note) =>
+    tags.every((tag) => note.tags.includes(tag))
+  );
+
+  if (filteredNotes.length == 0){
+    res.status(404).json({ error: "Note not found" });
+  } 
+  // Return the filtered notes
+  res.status(200).json(filteredNotes);
+});
+
+// Search notes by a phrase or keyword
+router.get("/search", (req, res) => {
+  if (!authenticate(req))
+    return;
+  const { q } = req.query;
+  
+  const tag_include = (element) => element.includes(q) === true;
+  // Filter the notes based on the keyword or phrase
+  const filteredNotes = example.notes.filter(
+    (note) =>
+      note.title.includes(q) ||
+      note.content.includes(q) ||
+      note.tags.some(tag_include)
+  );
+  
+  if (filteredNotes.length == 0){
+    res.status(404).json({ error: "Note not found" });
+  } 
+
+  // Return the filtered notes
+  res.status(200).json(filteredNotes);
+});
+
+
+
 // Create a new note
-app.post('/', (req, res) => {
+// Checked
+router.post('/', (req, res) => {
 
     if (!authenticate(req))
         return;
@@ -51,113 +97,89 @@ app.post('/', (req, res) => {
   });
 
 // Get a note by its id
-app.get('/notes/:noteID', (req, res) => {
-
-    if (!authenticate(req))
-        return;
-    const { noteID } = req.params;
+// Checked
+router.get('/:noteID', (req, res) => {
+  if (!authenticate(req))
+    return;
+  let noteID = req.params.noteID;
   
     // Find the note with the specified ID
-    const note = notes.find((note) => note.noteID === noteID);
+  const note = example.notes.find((note) => note.noteID == noteID);
   
     // Check if the note exists
-    if (!note) {
+  if (!note) {
       return res.status(404).json({ error: "Note not found" });
-    }
+  }
   
     // Return the note
     res.status(200).json(note);
-  });
+});
 
 // Update a note by its id
-app.put("/notes/:noteID", (req, res) => {
+// Checked
+router.put("/:noteID", (req, res) => {
 
-    if (!authenticate(req))
-        return;
-    const { noteID } = req.params;
-    const { title, content, tags } = req.body;
+  if (!authenticate(req))
+      return;
+
+  const { noteID, title, content, tags } = req.body;
+
+  // Validate the required parameters
+  if (!noteID || !title || !content || !tags) {
+    return res.status(400).json({ error: 'Missing required parameters' });
+  }
+    
   
-    // Find the note with the specified ID
-    const noteIndex = example.notes.findIndex((note) => note.noteID === noteID);
+  // Find the note with the specified ID
+  const noteIndex = example.notes.findIndex((note) => note.noteID == noteID);
   
-    // Check if the note exists
-    if (noteIndex === -1) {
-      return res.status(404).json({ error: "Note not found" });
-    }
+  // Check if the note exists
+  if (noteIndex == -1) {
+    return res.status(404).json({ error: "Note not found" });
+  }
   
-    // Update the note
-    example.notes[noteIndex] = {
-      noteID,
-      title,
-      content,
-      tags,
-    };
-  
-    // Return the updated note
-    res.status(200).json({
-      noteID,
-      message: "Note updated successfully",
-    });
+  // Update the note
+  example.notes[noteIndex] = {
+    noteID,
+    title,
+    content,
+    tags,
+  };
+
+  // Return the updated note
+  res.status(200).json({
+    noteID,
+    message: "Note updated successfully",
   });
+});
 
 // Delete a note by its id
-app.delete("/notes/:noteID", (req, res) => {
-    if (!authenticate(req))
-        return;
-    const { noteID } = req.params;
+// Checked
+router.delete("/:noteID", (req, res) => {
+  if (!authenticate(req))
+    return;
+  let noteID  = req.params.noteID;
   
-    // Find the note with the specified ID
-    const noteIndex = example.notes.findIndex((note) => note.noteID === noteID);
+  // Find the note with the specified ID
+  const noteIndex = example.notes.findIndex((note) => note.noteID == noteID);
   
-    // Check if the note exists
-    if (noteIndex === -1) {
-      return res.status(404).json({ error: "Note not found" });
-    }
+  // Check if the note exists
+  if (noteIndex === -1) {
+    return res.status(404).json({ error: "Note not found" });
+  }
   
-    // Remove the note from the array
-    const deletedNote = examples.notes.splice(noteIndex, 1)[0];
+  // Remove the note from the array
+  const deletedNote = example.notes.splice(noteIndex, 1)[0];
   
-    // Return the deleted note
-    res.status(200).json({
-      noteID: deletedNote.noteID,
-      message: "Note deleted successfully",
-    });
+  // Return the deleted note
+  res.status(200).json({
+    noteID: deletedNote.noteID,
+    message: "Note deleted successfully",
   });
+});
 
-// Search notes by a phrase or keyword
-app.get("/notes/search", (req, res) => {
-    if (!authenticate(req))
-        return;
-    const { q } = req.query;
-  
-    // Filter the notes based on the keyword or phrase
-    const filteredNotes = notes.filter(
-      (note) =>
-        note.title.includes(q) ||
-        note.content.includes(q) ||
-        note.tags.includes(q)
-    );
-  
-    // Return the filtered notes
-    res.status(200).json(filteredNotes);
-  });
 
-// Search notes by tags
-app.get("/notes/searchByTag", (req, res) => {
-    if (!authenticate(req))
-        return;
-    const { tag } = req.query;
-  
-    // Split the tag parameter into an array of tags
-    const tags = tag.split(",");
-  
-    // Filter the notes based on the tags
-    const filteredNotes = notes.filter((note) =>
-      tags.every((tag) => note.tags.includes(tag))
-    );
-  
-    // Return the filtered notes
-    res.status(200).json(filteredNotes);
-  });
+
+
 
 module.exports = router;
--- Milestone1/frontend/src/note/script.js
+++ Milestone1/frontend/src/note/script.js
@@ -1,11 +1,46 @@
 import '@material/web/button/filled-button'
 
-const imgContainer = document.querySelector('#img-container')
+var currentIndex = 1;
+var imageContainer = document.getElementsById("img-container");
+var imageElement = document.getElementById("img");
+var prevButton = document.getElementById("prev-btn");
+var nextButton = document.getElementById("next-btn");
+const slide_length = 15;
+var images =[];
 
-
-for (let i = 1; i < 16; i++) {
-    const img = document.createElement('img')
+for (let i = 1; i <= slide_length; i++){
+    var img = document.createElement('img')
     img.src = "../note/images/" + i.toString() + ".jpg";
-    img.style = "width: 100%"
-    imgContainer.appendChild(img)
+    img.style = "width: 100%";
+    images.push(img);
 }
+
+
+// Function to update the image source
+function updateImage() {
+    imageElement.remove();
+    imageContainer.appendChild(images[currentIndex]);
+    var imageElement = document.getElementById("img");
+}
+  
+  // Function to handle the previous button click
+function prevImage() {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = images.length - 1;
+    }
+    updateImage();
+}
+  
+  // Function to handle the next button click
+function nextImage() {
+    if (currentIndex < images.length - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+    updateImage();
+}
+
+updateImage();
