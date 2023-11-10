3cf452b4b196321946ec76c3de607dc7aff26afa
Deci Horine <djhorine@ncsu.edu>
2023-11-09 20:42:30 -0500

    Fix bug in con provider

--- Milestone2/api/src/accessObjects/connectionProvider.js
+++ Milestone2/api/src/accessObjects/connectionProvider.js
@@ -1,13 +1,12 @@
-const { query } = require('express');
 const mySql = require('mysql');
 let connection;
 
- /**
-     * Get the current database connection, or if one doesn't exist,
-     * connect to the database and return the new connection
-     * 
-     * @returns {Promise<mySql.Connection>} a promise of a database connection
-     */
+/**
+    * Get the current database connection, or if one doesn't exist,
+    * connect to the database and return the new connection
+    * 
+    * @returns {Promise<mySql.Connection>} a promise of a database connection
+    */
 function getCon() {
     return new Promise((res, rej) => {
         if (connection === undefined) {
@@ -21,8 +20,10 @@ function getCon() {
             });
 
             connection.connect((err) => {
-               console.log(`CRITICAL: Failed to connect to db (${err.code})`);
-               rej(err);
+                if (err) {
+                    console.log(`CRITICAL: Failed to connect to db (${err})`);
+                    rej(err);
+                }
             });
         }
 
@@ -33,30 +34,30 @@ function getCon() {
 /**
  * Make a database query
  * 
- * @param {string} query the query to execute
+ * @param {string} qString the query string to execute
  * @param {[]} params query parameters
  * @returns {Promise<{results, fields}>} the results and fields 
  */
-function query(query, params = []) {
+function query(qString, params = []) {
     return new Promise((res, rej) => {
         this.getCon().then(con => {
-            con.query(query, params, (err, results, fields) => {
+            con.query(qString, params, (err, results, fields) => {
                 if (err) {
                     if (err.code == 'PROTOCOL_CONNETION_LOST') {
                         console.log(`ERROR: Database connection lost, attempting to reconnect`);
                         connection = undefined;
-                        getCon().then(() => query(query, params));
+                        getCon().then(() => query(qString, params));
                     }
                     else {
-                        console.log(`ERROR: Failed to execute '${query}' (${err.code})`);
+                        console.log(`ERROR: Failed to execute '${qString}' (${err.code})`);
                         rej(err);
                     }
                 }
 
-                res({results, fields});
+                res({ results, fields });
             })
         })
     });
 }
 
-module.exports = {getCon, query}
+module.exports = { getCon, query }
