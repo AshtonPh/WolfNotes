02439cdf1396bf83765af9d65d13704a35c0e8d8
Zelda <wlu27@ncsu.edu>
2023-11-10 16:20:52 -0500

    changes on the auth backend and update the database

--- Milestone2/api/src/routes/authenticationRouter.js
+++ Milestone2/api/src/routes/authenticationRouter.js
@@ -10,37 +10,39 @@ router.get('/', TokenMiddleware, (req, res) => { // base API path, testing
 //register, add a user
 router.post('/register', (req, res) => {
 
-    userDao.addUser(req.body).then(result => { // try adding
+    userDao.addUser(req.body).then(result => { // try adding  a user
         generateToken(req, res, result);
         res.json(result);
     }).catch(error => {
-        res.status(401).json({error: error}); //catch and send error
+        res.status(400).json({error: error}); //catch and send error
     })
 });
 
-//login in user
+//users login in 
 router.post('/:userID/login', (req, res) => {
-    let username = req.body.username.trim();
-    let password = req.body.password.trim();
-    if(username == "" || password == "") { // user cannot be empty
+    let userName = req.body.userName.trim();
+    let passwordHash = req.body.passwordHash.trim();
+    if(userName == "" || passwordHash == "") { // user cannot be empty
         res.status(400).json({error: "fields cannot be empty!"});
     }
     else {
-        UserDAuserDao.getUserByCredentials(username, password).then(user => { // find user
+        userDao.getUserByCredentials(userName, passwordHash).then(user => { // find user to see if they exist 
             generateToken(req, res, user);
-            res.json({result: 'success'});
+            res.json({result: 'Logged in'});
         }).catch(error => {
+            //user does not exist or the user password and username does not match
             res.status(401).json({error: error});
         });
     } 
 });
 
+//users log out
 router.post('/:userID/logout', (req, res) => {
     removeToken(req, res);
-    res.json({message: "success"});
+    res.json({message: "Logged out"});
 });
 
-router.get('/:userID/current', AuthenticationMiddleware, (req, res) => {
+router.get('/:userID/current', TokenMiddleware, (req, res) => {
     res.json({message: 'success', user: req.user});
 })
 
